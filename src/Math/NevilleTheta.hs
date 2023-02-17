aca3516d505ee6309006927ab99a117ac0b5f68d
Stéphane Laurent <laurent_step@yahoo.fr>
2023-02-17 09:54:44 +0100

    squared modulus

--- src/Math/NevilleTheta.hs
+++ src/Math/NevilleTheta.hs
@@ -2,14 +2,28 @@ module Math.NevilleTheta
     ( theta_c, 
       theta_d,
       theta_n,
-      theta_s
+      theta_s,
+      theta_c', 
+      theta_d',
+      theta_n',
+      theta_s'
     ) where
 import Data.Complex
+import Math.EllipticIntegrals (ellipticF)
 import Math.JacobiTheta
 
 type Cplx = Complex Double
 
--- | Neville theta-c function
+i_ :: Cplx
+i_ = 0.0 :+ 1.0
+
+tauFromM :: Cplx -> Cplx
+tauFromM m = i_ * ellipticF (pi/2) (1 - m) / ellipticF (pi/2) m
+
+nomeFromM :: Cplx -> Cplx
+nomeFromM m = exp (i_ * pi * tauFromM m)
+
+-- | Neville theta-c function in terms of the nome
 theta_c :: 
      Cplx -- ^ z
   -> Cplx -- ^ q, the nome
@@ -20,7 +34,7 @@ theta_c z q =
     j3 = jtheta3 0 q
     z' = z / (j3 * j3)
 
--- | Neville theta-d function
+-- | Neville theta-d function in terms of the nome
 theta_d :: 
      Cplx -- ^ z
   -> Cplx -- ^ q, the nome
@@ -31,7 +45,7 @@ theta_d z q =
     j3 = jtheta3 0 q
     z' = z / (j3 * j3)
 
--- | Neville theta-n function
+-- | Neville theta-n function in terms of the nome
 theta_n :: 
      Cplx -- ^ z
   -> Cplx -- ^ q, the nome
@@ -42,7 +56,7 @@ theta_n z q =
     j3 = jtheta3 0 q
     z' = z / (j3 * j3)
 
--- | Neville theta-d function
+-- | Neville theta-d function in terms of the nome
 theta_s :: 
      Cplx -- ^ z
   -> Cplx -- ^ q, the nome
@@ -53,3 +67,31 @@ theta_s z q =
     j3 = jtheta3 0 q
     j3sq = j3 * j3
     z' = z / j3sq
+
+-- | Neville theta-c function in terms of the squared modulus
+theta_c' :: 
+     Cplx -- ^ z
+  -> Cplx -- ^ m, the squared modulus
+  -> Cplx
+theta_c' z m = theta_c z (nomeFromM m)
+
+-- | Neville theta-d function in terms of the squared modulus
+theta_d' :: 
+     Cplx -- ^ z
+  -> Cplx -- ^ m, the squared modulus
+  -> Cplx
+theta_d' z m = theta_d z (nomeFromM m)
+
+-- | Neville theta-n function in terms of the squared modulus
+theta_n' :: 
+     Cplx -- ^ z
+  -> Cplx -- ^ m, the squared modulus
+  -> Cplx
+theta_n' z m = theta_n z (nomeFromM m)
+
+-- | Neville theta-s function in terms of the squared modulus
+theta_s' :: 
+     Cplx -- ^ z
+  -> Cplx -- ^ m, the squared modulus
+  -> Cplx
+theta_s' z m = theta_s z (nomeFromM m)
