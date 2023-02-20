811154e76d9ee4d61c958faffc5427fa608551b8
Stéphane Laurent <laurent_step@yahoo.fr>
2023-02-20 18:52:11 +0100

    removed unused function

--- tests/Approx.hs
+++ tests/Approx.hs
@@ -10,10 +10,6 @@ approx0 n x = fromInteger (round $ x * (10^n)) / (10.0^^n)
 approx :: Int -> Complex Double -> Complex Double
 approx n z = approx0 n (realPart z) :+ approx0 n (imagPart z)
 
--- check whether rounded z1 and rounded z2 are equal
-approxEqual :: Int -> Complex Double -> Complex Double -> Bool
-approxEqual n z1 z2 = magnitude (approx n z1 - approx n z2) == 0.0
-
 assertApproxEqual :: String -> Int -> Complex Double -> Complex Double -> Assertion
 assertApproxEqual prefix n z1 z2 = 
   assertEqual prefix (approx n z1) (approx n z2)
